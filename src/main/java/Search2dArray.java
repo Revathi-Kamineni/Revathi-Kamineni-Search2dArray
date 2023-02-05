d17566f8976062fbd65f0fcd9f3eb35d6d7b3c8f
Revathi-Kamineni <revathikamineni77@gmail.com>
2023-02-05 17:46:57 +0000

    2d array

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,29 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+        if(nums==null || nums.length==0 || nums[0].length==0) 
+        return false;
+
+    int m = nums.length;
+    int n = nums[0].length;
+
+    int start = 0;
+    int end = m*n-1;
+
+    while(start<=end){
+        int mid=(start+end)/2;
+        int midX=mid/n;
+        int midY=mid%n;
+
+        if(nums[midX][midY]==target) 
+            return true;
+
+        if(nums[midX][midY]<target){
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+    }
         return false;
     }
 }
